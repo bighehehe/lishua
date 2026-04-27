9b69c034a8250b80085e52aa02f20235fe51c4ee
bighehehe <1960075756@qq.com>
2026-04-27 13:39:00 +0800

    接入飞书

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,13 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
+import vercel from '@astrojs/vercel';
 
 // https://astro.build/config
 export default defineConfig({
+  devToolbar: {
+    enabled: false
+  },
+  output: 'static',
+  adapter: vercel(),
   integrations: [tailwind()]
 });
