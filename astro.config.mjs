cecdea399af2de3661f7fb7c639e66f405ec64f7
bighehehe <1960075756@qq.com>
2026-05-26 16:59:55 +0800

    优化页面配置与多处布局调整

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,15 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import vercel from '@astrojs/vercel';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://www.leeshua.com',
   devToolbar: {
     enabled: false
   },
   output: 'static',
   adapter: vercel(),
-  integrations: [tailwind()]
+  integrations: [tailwind(), sitemap()]
 });
